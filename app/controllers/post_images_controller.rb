5a048d520102c001ca583c591b12409316722bfa
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-11-16 10:40:31 +0000

    Finish[4-6]

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,8 +7,11 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)#投稿データとして許可されているか
     @post_image.user_id = current_user.id#ログインユーザーのidを取得
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
   
   def index
